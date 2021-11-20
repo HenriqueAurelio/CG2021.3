8f1a943eb1c86f283216107756139ba5d4e730d4
LeonardoN-Aragao <Leonardonunesaragao@gmail.com>
2021-11-20 16:53:37 -0300

    Import car class to basic scene

--- works/basicScene.js
+++ works/basicScene.js
@@ -9,6 +9,8 @@ import {
   initDefaultBasicLight,
 } from '../libs/util/util.js'
 
+import car from './car.js'
+
 var stats = new Stats() // To show FPS information
 var scene = new THREE.Scene() // Create main scene
 var renderer = initRenderer() // View function in util/utils
@@ -32,38 +34,6 @@ var plane = new THREE.Mesh(planeGeometry, planeMaterial)
 // add the plane to the scene
 scene.add(plane)
 
-//Adding car to the scene
-var tire1 = createTire()
-scene.add(tire1)
-var tire2 = createTire()
-scene.add(tire2)
-var tire3 = createTire()
-scene.add(tire3)
-var tire4 = createTire()
-scene.add(tire4)
-var axis1 = createAxis()
-scene.add(axis1)
-var axis2 = createAxis()
-scene.add(axis2)
-
-//Grouping tires with axis
-axis1.add(tire1)
-axis1.add(tire2)
-axis2.add(tire3)
-axis2.add(tire4)
-
-//Setting position to the axis and tire
-axis1.position.set(10, 0, 0.5)
-tire1.position.set(0, 2.0, 0)
-tire1.rotateX(Math.PI / 2)
-tire2.position.set(0, -2.0, 0)
-tire2.rotateX(Math.PI / 2)
-
-axis2.position.set(0, 0, 0.5)
-tire3.position.set(0, 2.0, 0)
-tire3.rotateX(Math.PI / 2)
-tire4.position.set(0, -2.0, 0)
-tire4.rotateX(Math.PI / 2)
 
 // Use this to show information onscreen
 var controls = new InfoBox()
@@ -84,23 +54,12 @@ window.addEventListener(
   false
 )
 
+new car(scene);
+
 render()
 
-function createTire() {
-  const geometry = new THREE.TorusGeometry(0.25, 0.08, 8, 100)
-  const material = new THREE.MeshPhongMaterial({ color: 'rgb(0, 0, 213)' })
-  const torus = new THREE.Mesh(geometry, material)
-  return torus
-}
 
-function createAxis() {
-  const CylinderGeometry = new THREE.CylinderGeometry(0.25, 0.25, 4)
-  const cylinderMaterial = new THREE.MeshPhongMaterial({
-    color: 'rgb(255, 0, 0)',
-  })
-  const cylinder = new THREE.Mesh(CylinderGeometry, cylinderMaterial)
-  return cylinder
-}
+
 function render() {
   stats.update() // Update FPS
   trackballControls.update() // Enable mouse movements
